5918dacb76cde54b8e745ddb7394e2d1279d83d3
fangshipt <fangshi205@gmail.com>
2025-10-07 14:49:51 +0700

    2%

--- src/main/java/fourpetals/com/config/SecurityConfig.java
+++ src/main/java/fourpetals/com/config/SecurityConfig.java
@@ -7,11 +7,10 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(auth -> auth.requestMatchers("/", "/home", "/guest/**", "/styles/**", "/css/**",
-				"/js/**", "/images/**", "/webjars/**").permitAll().anyRequest().authenticated())
+				"/js/**", "/images/**", "/webjars/**").permitAll().requestMatchers("/manager/**").permitAll().anyRequest().authenticated())
 				.formLogin(form -> form.loginPage("/login").permitAll()).logout(logout -> logout.permitAll())
 				.csrf(csrf -> csrf.disable());
 
